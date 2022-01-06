5abea441f1e9872493ccfc125fb0322e37111f64
dttrujillo00 <dttrujillo00@gmail.com>
2022-01-06 08:14:41 -0500

    Menu de pagina principal terminado

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const menu = document.querySelector('.menu');
 const secondIconMenu = document.querySelector('.menu > img');
 const body = document.querySelector('body');
 const closeMenuElement = document.querySelector('.hide-menu');
+const liArchivo = document.querySelector('.li-archivo');
+const archivoContent = document.querySelector('.archivo-content');
 
 const showMenu = () => {
     body.classList.add('opacity');
@@ -19,11 +21,18 @@ const hideMenu = () => {
     body.classList.remove('opacity');
     menu.classList.remove('active');
     secondIconMenu.classList.remove('active');
+    archivoContent.classList.remove('active');
+}
+
+const showArchivo = () => {
+    archivoContent.classList.toggle('active');
+    // console.log(liArchivo);
 }
 
 iconMenu.addEventListener('click',showMenu);
 secondIconMenu.addEventListener('click', hideMenu);
 closeMenuElement.addEventListener('click', hideMenu);
+liArchivo.addEventListener('click', showArchivo);
 
 /************************************************************
  * MANEJADOR DEL POSICIONAMIENTO DE LAS TARJETAS DE EMPRESA *
@@ -40,5 +49,5 @@ empresas.forEach((empresa) => {
 
 empresas.forEach((empresa, index) => {
     empresa.style.transform = `translate(${-20*(index + 1)}px)`;
-    console.log(empresa, index);
+    // console.log(empresa, index);
 });
