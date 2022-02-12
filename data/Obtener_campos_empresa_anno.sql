0b1e90373d5ca144344359b642e82dfa79ba4951
Henri Daniel Peña Dequero <henridanielpenadequero@gmail.com>
2022-02-12 17:26:38 +0100

    arreglado autoinvrement y mejorada consulta de obtener los campos por empresa y año con sumatoria

--- data/Obtener_campos_empresa_anno.sql
+++ data/Obtener_campos_empresa_anno.sql
@@ -1,4 +1,2 @@
-﻿SELECT field, SUM(amount) FROM
-(SELECT field, SUM(amount) as amount FROM business b INNER JOIN account a ON b.business_id = a.business_id INNER JOIN date d ON a.date_id = d.date_id INNER JOIN field f ON a.field_id = f.field_id WHERE b.business_name = ? AND d.year = ? AND a.is_positive = TRUE GROUP BY field
-UNION 
-SELECT field, SUM(-amount) as amount FROM business b INNER JOIN account a ON b.business_id = a.business_id INNER JOIN date d ON a.date_id = d.date_id INNER JOIN field f ON a.field_id = f.field_id WHERE b.business_name = ? AND d.year = ? AND a.is_positive = FALSE GROUP BY field) GROUP BY field
+﻿SELECT field, SUM( CASE a.is_positive WHEN TRUE THEN amount ELSE -amount END) as amount FROM business b INNER JOIN account a ON b.business_id = a.business_id INNER JOIN date d ON a.date_id = d.date_id INNER JOIN field f ON a.field_id = f.field_id WHERE b.business_name = ? AND d.year = ? GROUP BY field;
+
