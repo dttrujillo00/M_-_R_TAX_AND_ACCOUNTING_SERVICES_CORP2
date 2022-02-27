430ce73b6493168bf417cc9d27e37571980fffa3
anthuand <anthuandamas@gmail.com>
2022-02-26 23:23:48 -0500

    arreglado reguero de sql

--- preload.js
+++ preload.js
@@ -3,24 +3,11 @@
 
 const { contextBridge, ipcRenderer } = require("electron");
 
-// As an example, here we use the exposeInMainWorld API to expose the IPC renderer 
-// to the main window. They'll be accessible at "window.ipcRenderer".
+
 process.once("loaded", () => {
   contextBridge.exposeInMainWorld("ipcRenderer", ipcRenderer);
 });
 
-
-
-
-
-// const invoke = (async(event,param)=> 
-// {
-//     await ipcRenderer.invoke(event,param).then((result) => {
-//         console.log("Termino la consulta");
-//         console.log(result);
-//         return result;
-//     });
-// });
 
 
 
