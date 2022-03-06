ccfc5e1d13bd8d972210a27b0281949f0f0d21f3
dttrujillo00 <dttrujillo00@gmail.com>
2022-03-06 09:27:03 -0500

    Nothing important

--- js/script.js
+++ js/script.js
@@ -102,8 +102,7 @@ const posicionarTarjetasEmpresas = () => {
  * MANEJADOR PARA MODIFICAR LAS EMPRESAS *
  *  **************************************/
 const modal = document.querySelector('.modal');
-const btnAceptar = document.querySelector('.btn-aceptar');
-const btnCancelar = document.querySelector('.btn-cancelar');
+
 
 const showModal = nombre => {
     modal.children[1].innerText = nombre;
@@ -114,19 +113,26 @@ const hideModal = () => {
     modal.classList.remove('active');
 }
 
-btnAceptar.addEventListener('click', hideModal);
-btnCancelar.addEventListener('click', hideModal);
-
 // FUNCION ELIMINAR EMPRESA
-const eliminarEmpresa =  async(e) => {
+const eliminarEmpresa =  (e) => {
+    const btnAceptar = document.querySelector('.modal .btn-aceptar');
+    const btnCancelar = document.querySelector('.modal .btn-cancelar');
+
     const nombre = e.target.parentElement.title;
     const id = e.target.parentElement.id;
     console.log("eliminando id: "+e.target.parentElement.id);
     showModal(nombre);
 
-    await window.ipcRenderer.invoke('eliminar_empresa', parseInt(id));
-    console.log("se elimino correctamente")
-    return ;
+    btnAceptar.addEventListener('click', async () => {
+        await window.ipcRenderer.invoke('eliminar_empresa', parseInt(id));
+        console.log("se elimino correctamente")
+        return ;
+    });
+
+    btnCancelar.addEventListener('click', () => {
+        hideModal();
+    });
+
 }
 
 const manejadorModificarEmpresas = () => {
@@ -191,63 +197,6 @@ const manejadorModificarEmpresas = () => {
 }
 
 /***************************************
- * MANEJADOR PARA OBTENER LAS EMPRESAS *
- *  ************************************/
-const footer = document.querySelector('footer');
-let filaEmpresa;
-
-const crearElementoHTMLEmpresa = (nombre, id, index) => {
-    element = `
-    <li title="${nombre}" class="empresa" id="${id}">
-        <img src="../icons/delete.png" class="icon-delete">
-        <div class="empresa-name">
-            <input type="text" value="${nombre}" disabled>
-        </div> 
-    </li>
-    `;
-    return element;
-}
-
- const renderEmpresas = (empresas) => {
-    const emptyFooter = ``;
-    footer.innerHTML = emptyFooter;
-
-    empresas.forEach( (empresa,index) => {
-        if(index%11 === 0){
-            filaEmpresa = document.createElement("ul");
-            filaEmpresa.classList.add('fila-empresas');
-            footer.appendChild(filaEmpresa);
-        }
-
-
-        filaEmpresa.innerHTML += crearElementoHTMLEmpresa(empresa.business_name, empresa.business_id);
-        
-    });
-}
-
-const getEmpresas =async () => {
-
-    await window.ipcRenderer.invoke('obtener_empresas_por_anno',currentYear).then((result) => {
-        console.log("Termino la consulta");
-        console.log(result);
-	    renderEmpresas(result);
-        posicionarTarjetasEmpresas();
-        manejadorModificarEmpresas();
-    })
-    
-    // fetch('../empresas.json')
-    // .then(res => res.json())
-    // .then(data => {
-    //     renderEmpresas(data.empresas);
-    //     posicionarTarjetasEmpresas();
-    //     manejadorModificarEmpresas();
-    // });
-
-}
-
-
-
-/***************************************
  * MANEJADOR PARA AGREGAR LAS EMPRESAS *
  *  ************************************/
 const liAgregarEmpresa = document.querySelector('.agregar-empresa');
@@ -256,6 +205,7 @@ const agregarTarjeta = () => {
     hideMenu();
 
     filaEmpresa.innerHTML += crearElementoHTMLEmpresa('', '');
+    console.log(filaEmpresa);
     posicionarTarjetasEmpresas();
     manejadorModificarEmpresas();
 
@@ -291,11 +241,63 @@ const agregarTarjeta = () => {
 	        }
 
             const result = await window.ipcRenderer.invoke('insertar_empresa', business);
+            console.log(result);
         }
     });
 }
 
 liAgregarEmpresa.addEventListener('click', agregarTarjeta);
+
+/***************************************
+ * MANEJADOR PARA OBTENER LAS EMPRESAS *
+ *  ************************************/
+ const footer = document.querySelector('.footer');
+ let filaEmpresa;
+ 
+ const crearElementoHTMLEmpresa = (nombre, id) => {
+     element = `
+     <li title="${nombre}" class="empresa" id="${id}">
+         <img src="../icons/delete.png" class="icon-delete">
+         <div class="empresa-name">
+             <input type="text" value="${nombre}" disabled>
+         </div> 
+     </li>
+     `;
+     return element;
+ }
+ 
+  const renderEmpresas = (empresas) => {
+     const emptyFooter = ``;
+     footer.innerHTML = emptyFooter;
+
+     filaEmpresa = document.createElement("ul");
+     filaEmpresa.classList.add('fila-empresas');
+     footer.appendChild(filaEmpresa);
+ 
+     empresas.forEach( (empresa,index) => {
+ 
+         if(index%11 === 0){
+             filaEmpresa = document.createElement("ul");
+             filaEmpresa.classList.add('fila-empresas');
+             footer.appendChild(filaEmpresa);
+         }
+ 
+         filaEmpresa.innerHTML += crearElementoHTMLEmpresa(empresa.business_name, empresa.business_id);
+         
+     });
+ }
+ 
+ const getEmpresas =async () => {
+ 
+     await window.ipcRenderer.invoke('obtener_empresas_por_anno',currentYear).then((result) => {
+         console.log("Termino la consulta");
+         console.log(result);
+         renderEmpresas(result);
+         posicionarTarjetasEmpresas();
+         manejadorModificarEmpresas();
+     })
+ 
+ }
 
 
 (async function init() {
