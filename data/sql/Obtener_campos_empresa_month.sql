9aedfc59e7d5141e97bdd6e37c25af44a720eb8c
Henri Daniel Peña Dequero <henridanielpenadequero@gmail.com>
2022-04-09 05:47:25 +0200

    add get field by company and month

---
+++ data/sql/Obtener_campos_empresa_month.sql
@@ -0,0 +1,4 @@
+﻿SELECT field AS Field,
+SUM(CASE a.is_positive WHEN TRUE THEN amount ELSE -amount END) AS YDT 
+FROM business b INNER JOIN account a ON b.business_id = a.business_id INNER JOIN date d ON a.date_id = d.date_id INNER JOIN field f ON a.field_id = f.field_id 
+WHERE b.business_name = ? AND year = ? AND month = ? GROUP BY field
